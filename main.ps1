2b7c1b234c6d6ef1a24b70ab8a95f324353593dd
CodeurIII <nathan2.che974@gmail.com>
2026-05-06 17:48:28 +0400

    bruh

--- main.ps1
+++ main.ps1
@@ -1,17 +1,17 @@
 try { Stop-Process -Name "Discord" -Force -ErrorAction Stop } catch {}
 
 # Sync fork avec upstream (sans interaction)
-git fetch upstream
+# git fetch upstream
 
-# Vérifie si fast-forward possible
-$ffPossible = git merge-base --is-ancestor upstream/main main
-if ($LASTEXITCODE -eq 0) {
-    git checkout main
-    git merge --ff-only upstream/main
-    git push origin main
-} else {
-    Write-Host "Sync skipped: nécessite intervention (conflits ou divergence)."
-}
+# # Vérifie si fast-forward possible
+# $ffPossible = git merge-base --is-ancestor upstream/main main
+# if ($LASTEXITCODE -eq 0) {
+#     git checkout main
+#     git merge --ff-only upstream/main
+#     git push origin main
+# } else {
+#     Write-Host "Sync skipped: nécessite intervention (conflits ou divergence)."
+# }
 
 pnpm build
 pnpm inject -branch stable
